21e176228dcf6ea3382c6e215611be8691da374d
p7m <perpetuum7@protonmail.com>
2022-05-07 21:38:23 +0200

    feat: deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,64 @@
+import json
+
+from brownie import accounts
+from brownie.network.gas.strategies import GasNowScalingStrategy
+from brownie.project import load as load_project
+from brownie.project.main import get_loaded_projects
+
+
+DEPLOYER = accounts.load('p7m')
+REQUIRED_CONFIRMATIONS = 1
+
+# deployment settings
+# most settings are taken from `contracts/pools/{POOL_NAME}/pooldata.json`
+POOL_NAME = "three-pool"
+
+# temporary owner address
+POOL_OWNER = "0xC4d54E7e94B68d88Ad7b00d0689669d520cce2fB"
+
+
+def _tx_params():
+    return {
+        "from": DEPLOYER,
+        "required_confs": REQUIRED_CONFIRMATIONS
+    }
+
+
+def main():
+    project = get_loaded_projects()[0]
+    balance = DEPLOYER.balance()
+
+    # load data about the deployment from `pooldata.json`
+    contracts_path = project._path.joinpath("contracts/pools")
+    with contracts_path.joinpath(f"{POOL_NAME}/pooldata.json").open() as fp:
+        pool_data = json.load(fp)
+
+    swap_name = next(i.stem for i in contracts_path.glob(f"{POOL_NAME}/StableSwap*"))
+    swap_deployer = getattr(project, swap_name)
+    token_deployer = getattr(project, pool_data.get("lp_contract"))
+
+    underlying_coins = [i["underlying_address"] for i in pool_data["coins"]]
+    wrapped_coins = [i.get("wrapped_address", i["underlying_address"]) for i in pool_data["coins"]]
+
+    # deploy the token
+    token_args = pool_data["lp_constructor"]
+    token = token_deployer.deploy(token_args["name"], token_args["symbol"], _tx_params())
+
+    # deploy the pool
+    abi = next(i["inputs"] for i in swap_deployer.abi if i["type"] == "constructor")
+    args = pool_data["swap_constructor"]
+    args.update(
+        _coins=wrapped_coins,
+        _underlying_coins=underlying_coins,
+        _pool_token=token,
+        _owner=POOL_OWNER,
+        _fee_collector=POOL_OWNER,
+    )
+    deployment_args = [args[i["name"]] for i in abi] + [_tx_params()]
+
+    swap = swap_deployer.deploy(*deployment_args)
+
+    # set the minter
+    token.set_minter(swap, _tx_params())
+
+    print(f"Gas used in deployment: {(balance - DEPLOYER.balance()) / 1e18:.4f} ETH")
