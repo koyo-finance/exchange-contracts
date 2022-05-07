38c79aeda910c756a339e8f078972aec938bc995
p7m <perpetuum7@protonmail.com>
2022-05-07 14:58:37 +0200

    chore: scaffold

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-vyper';
+import '@typechain/hardhat';
+import 'hardhat-abi-exporter';
+import type { HardhatUserConfig } from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+	vyper: {
+		version: '0.3.1'
+	},
+	abiExporter: {
+		path: './abis',
+		runOnCompile: true,
+		clear: true,
+		flat: true,
+		only: []
+	},
+	typechain: {
+		outDir: 'typechain',
+		target: 'ethers-v5',
+		alwaysGenerateOverloads: true,
+		externalArtifacts: ['abis/*.json']
+	}
+};
+
+export default config;
