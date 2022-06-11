41ffe2778d46c7bb0ed3f1317171ef7aea70ab10
p7m <perpetuum7@protonmail.com>
2022-06-11 15:17:43 +0200

    feat: list raw fee value

--- scripts/pools/read_fees.py
+++ scripts/pools/read_fees.py
@@ -7,8 +7,8 @@ POOL_ADDRESS = "0x9F0a572be1Fcfe96E94C0a730C5F4bc2993fe3F6"
 def main():
     pool = StableSwap4Pool.at(POOL_ADDRESS)
 
-    print(f"fee - {pool.fee() / 1e8}%")
-    print(f"admin fee - {pool.admin_fee() / 1e8}% of {pool.fee() / 1e8}%")
+    print(f"fee - {pool.fee() / 1e8}% ({pool.fee()})")
+    print(f"admin fee - {pool.admin_fee() / 1e8}% of {pool.fee() / 1e8}% ({pool.admin_fee()})")
 
     print("FRAX - ", pool.admin_balances(0) / 10 ** 18)
     print("DAI  - ", pool.admin_balances(1) / 10 ** 18)
