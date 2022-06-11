c35a92c36bed9c76df4340bff2230d5b90d29c1f
p7m <perpetuum7@protonmail.com>
2022-06-11 12:22:07 +0200

    feat: read 4pool fees

--- scripts/pools/read_fees.py
+++ scripts/pools/read_fees.py
@@ -1,13 +1,16 @@
 from brownie import StableSwap4Pool
 
 
-POOL_ADDRESS = "0x9f0a572be1fcfe96e94c0a730c5f4bc2993fe3f6"
+POOL_ADDRESS = "0x9F0a572be1Fcfe96E94C0a730C5F4bc2993fe3F6"
 
 
 def main():
     pool = StableSwap4Pool.at(POOL_ADDRESS)
 
-    print(pool.admin_balances(0) / 10 ** 18)
-    print(pool.admin_balances(1) / 10 ** 18)
-    print(pool.admin_balances(2) / 10 ** 6)
-    print(pool.admin_balances(3) / 10 ** 6)
+    print(f"fee - {pool.fee() / 1e8}%")
+    print(f"admin fee - {pool.admin_fee() / 1e8}% of {pool.fee() / 1e8}%")
+
+    print("FRAX - ", pool.admin_balances(0) / 10 ** 18)
+    print("DAI  - ", pool.admin_balances(1) / 10 ** 18)
+    print("USDC - ", pool.admin_balances(2) / 10 ** 6)
+    print("USDT - ", pool.admin_balances(3) / 10 ** 6)
